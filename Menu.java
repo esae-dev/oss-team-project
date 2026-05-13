d6887a1e5c954b64ff3c07bcebb58636bdfaaafa
sspark0817 <sspark0817@naver.com>
2026-05-13 22:30:34 +0900

    Menu 클래스 구현

--- Menu.java
+++ Menu.java
@@ -1,3 +1,43 @@
 public class Menu {
-    // TODO: 메뉴판 출력 기능 구현 예정
+
+    private String[] menuNames = {
+            "아메리카노",
+            "카페라떼",
+            "카푸치노",
+            "치즈버거",
+            "감자튀김"
+    };
+
+    private int[] menuPrices = {
+            2000,
+            3000,
+            3500,
+            5500,
+            2500
+    };
+
+    // 메뉴판 출력
+    public void printMenu() {
+
+        System.out.println("===== 메뉴판 =====");
+
+        for(int i = 0; i < menuNames.length; i++) {
+            System.out.println((i + 1) + ". " + menuNames[i] + " - "  + menuPrices[i]  + "원");
+        }
+    }
+
+    // 메뉴 이름 반환
+    public String getMenuName(int index) {
+        return menuNames[index];
+    }
+
+    // 메뉴 가격 반환
+    public int getMenuPrice(int index) {
+        return menuPrices[index];
+    }
+
+    // 메뉴 개수 반환
+    public int getMenuCount() {
+        return menuNames.length;
+    }
 }
