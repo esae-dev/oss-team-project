6c6485dbbe72f8d7c91767455d7d5116311168ec
esae-dev <zern35@gmail.com>
2026-05-14 17:20:29 +0900

    Main.java 메뉴 출력 연결

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Mini Kiosk Program");
 
-        // TODO: Menu, Order, Receipt, InputCheck 기능 연결 예정
+         Menu menu = new Menu();
+         menu.printMenu();
+
+        // TODO: Order, Receipt, InputCheck 기능 연결 예정
     }
 }
