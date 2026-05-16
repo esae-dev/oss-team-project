aa919058aced9e645396b0a38854938426ea2b07
sspark0817 <sspark0817@naver.com>
2026-05-17 02:48:15 +0900

    feature: 메뉴 번호 입력 검사 기능 추가

--- InputCheck.java
+++ InputCheck.java
@@ -1,3 +1,12 @@
 public class InputCheck {
     // TODO: 잘못된 입력 처리 기능 구현 예정
+    public boolean isValidMenu(int menuNumber, int menuCount) {
+
+    if (menuNumber < 1 || menuNumber > menuCount) {
+        System.out.println("잘못된 메뉴 번호입니다.");
+        return false;
+    }
+
+    return true;
+    }
 }
