281aed671bd36153d6be8c9a8b9c382b563f7f12
Choi Min Seo <zern35@gmail.com>
2026-05-17 15:57:47 +0900

    Main.java 주문 흐름 연결

--- Main.java
+++ Main.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Menu menu = new Menu();
+        Order order = new Order();
+        InputCheck inputCheck = new InputCheck();
+        Receipt receipt = new Receipt();
+
         System.out.println("Mini Kiosk Program");
 
-         Menu menu = new Menu();
-         menu.printMenu();
+        while (true) {
+            menu.printMenu();
 
-        // TODO: Order, Receipt, InputCheck 기능 연결 예정
+            System.out.print("메뉴 번호를 입력하세요 (0 입력 시 주문 종료): ");
+            int menuNumber = scanner.nextInt(); //일단 번호로만 입력받음(추후 업데이트 예정)
+
+            if (menuNumber == 0) {
+                break;
+            }
+
+            if (!inputCheck.isValidMenu(menuNumber, menu.getMenuCount())) {
+                continue;
+            }
+
+            int menuIndex = menuNumber - 1;
+
+            String menuName = menu.getMenuName(menuIndex);
+            int menuPrice = menu.getMenuPrice(menuIndex);
+
+            order.addOrder(menuName, menuPrice);
+            System.out.println();
+        }
+
+        receipt.printReceipt(order);
+
+        scanner.close();
     }
 }
