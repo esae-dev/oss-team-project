76aeb45f8a9f472d9e6f7e84df45405ccd228916
sspark0817 <sspark0817@naver.com>
2026-05-23 13:14:17 +0900

    HOT/ICED 옵션 추가

--- Option.java
+++ Option.java
@@ -1,3 +1,7 @@
 public class Option {
-
+      public void printOptions() {
+        System.out.println("===== 음료 옵션 =====");
+        System.out.println("1. HOT");
+        System.out.println("2. ICED");
+    }
 }
