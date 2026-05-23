4ccf73ce25534b3b613d6fcd1428b7b11257703a
sspark0817 <sspark0817@naver.com>
2026-05-23 13:16:58 +0900

    선택한 옵션 문자열 반환 기능 구현

--- Option.java
+++ Option.java
@@ -4,4 +4,14 @@ public class Option {
         System.out.println("1. HOT");
         System.out.println("2. ICED");
     }
+
+      public String getOption(int choice) {
+        if (choice == 1) {
+            return "HOT";
+        }
+        if (choice == 2) {
+            return "ICED";
+        }
+        return "INVALID";
+    }
 }
